7fbe82324543cb2173dcbe9f0aa1e96343ee7bf5
Jose V Sebastian <josevseb99@gmail.com>
2024-01-19 01:29:23 +0530

    ci: setup release pipeline

---
+++ release.config.cjs
@@ -0,0 +1,30 @@
+/** @type {import('semantic-release').GlobalConfig} */
+const config = {
+  branches: [
+    "+([0-9])?(.{+([0-9]),x}).x",
+    "main",
+    "next",
+    "next-major",
+    { name: "beta", prerelease: true },
+    { name: "alpha", prerelease: true },
+  ],
+  plugins: [
+    ["@semantic-release/commit-analyzer", { preset: "conventionalcommits" }],
+    [
+      "@semantic-release/release-notes-generator",
+      { preset: "conventionalcommits" },
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogTitle:
+          "# Changelog\n\nAll notable changes to this project will be documented in this file. See\n[Conventional Commits](https://conventionalcommits.org) for commit guidelines.",
+      },
+    ],
+    ["@semantic-release/npm", { tarballDir: "pack" }],
+    "@semantic-release/git",
+    ["@semantic-release/github", { assets: "pack/*.tgz" }],
+  ],
+};
+
+module.exports = config;
